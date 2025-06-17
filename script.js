ce3a2f9c7c574cbb408665d93e51920965c5e73d
pedrosortega <psortega2004@gmail.com>
2025-06-17 15:20:24 -0400

    solved myFilter

--- script.js
+++ script.js
@@ -16,6 +16,23 @@ function myMap(arr, func) {
     }
 }
 let myMapAr = [1, 2, 3, 4, 5];
-myEach(myMapAr, (element) => {
+myMap(myMapAr, (element) => {
     console.log(element * 2);
 });
+
+console.log('----------------------------------------------');
+
+function myFilter(arr, func) {
+    for (let i = 0; i < arr.length; i++) {
+        func(arr[i]);
+    }
+}
+let filterArr = [1, 2, 3, 4, 5];
+let result=[];
+myFilter(filterArr, (element) => {
+    if(element%2===0){
+        result.push(element);
+    }
+});
+
+console.log(result);
