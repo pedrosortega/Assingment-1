a882b98ca424ac4791f7df350c760b14cd1f8496
Elian Echavarria <elianstanly431@gmail.com>
2025-06-17 14:41:36 -0400

    solve myMap - Elian

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 function myEach(arr, func) {
-for (let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
         func(arr[i]);
     }
 }
 let ar = [1, 2, 3, 4, 5];
 myEach(ar, (element) => {
-console.log(element);
+    console.log(element);
+});
+
+console.log('----------------------------------------------');
+
+function myMap(arr, func) {
+    for (let i = 0; i < arr.length; i++) {
+        func(arr[i]);
+    }
+}
+let myMapAr = [1, 2, 3, 4, 5];
+myEach(myMapAr, (element) => {
+    console.log(element * 2);
 });
